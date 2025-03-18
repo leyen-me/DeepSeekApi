b2f9e5ee695eb962ef09895bf77757a11cccaac2
DIFFFFFFT <672228275@qq.com>
2025-03-18 10:25:00 +0800

    change mode

--- constans/__init__.py
+++ constans/__init__.py
@@ -2,11 +2,11 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    base_url='https://api-inference.modelscope.cn/v1/',
-    api_key=os.getenv('MODEL_SCOPE_API_KEY'),
+    base_url='https://dashscope.aliyuncs.com/compatible-mode/v1',
+    api_key=os.getenv('ALIYUN_API_KEY'),
 )
 
-base_model = "deepseek-ai/DeepSeek-V3"
+base_model = "deepseek-v3"
 
 def build_tool_response(tool_result):
     return f"tool:\n{tool_result}"
